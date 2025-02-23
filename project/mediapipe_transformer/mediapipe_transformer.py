27f1ec0d29e0d97a7eb45332d4d901fa78d8023f
Полонский Святослав Игоревич <sipolonskii@edu.hse.ru>
2025-02-23 17:53:43 +0300

    Works with batches with size 1

--- project/mediapipe_transformer/mediapipe_transformer.py
+++ project/mediapipe_transformer/mediapipe_transformer.py
@@ -22,29 +22,33 @@ class MediapipeTransformer():
         '''
         Checks that has left hand landmarks
         '''
-        return not self.both_hand(multi_handedness) and not self.no_hands(multi_handedness) and multi_handedness[0].label == "Left"
+        return not self.both_hand(multi_handedness) and not self.no_hands(multi_handedness) and multi_handedness[0].classification[0].label  == "Left"
 
     def right_hand_only(self, multi_handedness):
         '''
         Checks that has right hand landmarks
         '''
-        return not self.both_hand(multi_handedness) and not self.no_hands(multi_handedness) and multi_handedness[0].label == "Right"
+        return not self.both_hand(multi_handedness) and not self.no_hands(multi_handedness) and multi_handedness[0].classification[0].label  == "Right"
 
     def landmarcks_tovec(self, multi_hands_landmarks, multi_handedness):
         '''
         Transforms landmarks to tensor with size 1 x 126
         '''
         res = []
+        if multi_handedness == None:
+            res = [0.] * 126
+            return torch.tensor(res)
+        
         for landmarks in multi_hands_landmarks:
             for landmark in landmarks.landmark:
                 res.extend([landmark.x, landmark.y, landmark.z])
                 
         if self.no_hands(multi_handedness):
-            res = [0] * 126
+            res = [0.] * 126
         elif self.left_hand_only(multi_handedness):
-            res += [0] * 63
+            res += [0.] * 63
         elif self.right_hand_only(multi_handedness):
-            res = [0] * 63 + res
+            res = [0.] * 63 + res
             
         return torch.tensor(res)
     
@@ -52,9 +56,11 @@ class MediapipeTransformer():
         '''
         Returns tensor of landmarks for hands in photo
         '''
-        img_np = (img.permute(1, 2, 0).numpy() * 255).astype(np.uint8)
-        processed_img = self.hands.process(img_np)
-        return self.landmarcks_tovec(processed_img.multi_hand_landmarks, processed_img.multi_handedness)
+        img_np = (img.permute(0, 2, 3, 1) * 255).to(dtype=torch.uint8)
+
+        processed_img = self.hands.process(img_np[0])
+        return self.landmarcks_tovec(processed_img.multi_hand_landmarks, processed_img.multi_handedness).unsqueeze(0)
+
     
     def __del__(self):
         self.hands.close()
